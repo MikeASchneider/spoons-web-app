37ca6dfb7ff67e686f1c7772ac1b46c67acbc0ed
Sam Mirza <sam@sammirza.co.uk>
2012-05-15 22:24:52 +0100

    removed indent from PHP code block causing indentation within textarea

--- manual.php
+++ manual.php
@@ -25,10 +25,10 @@
 	<p>On this page, you can manually modify the spoons list. Example uses: Fix a typo, re-add an accidentally eliminated player, etc.</p>
 	<form method="post" action="manual.php">
 		<textarea name="spoonlist" rows="30" cols="40">
-			<?php 
-				$spoonslist = file_get_contents($listfilename);
-				echo trim(htmlspecialchars($spoonslist));
-			?>
+<?php 
+			$spoonslist = file_get_contents($listfilename);
+			echo trim(htmlspecialchars($spoonslist));
+?>
 		</textarea>
 		<button type="submit">Submit</button>
 	</form>
